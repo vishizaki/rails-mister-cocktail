126c3d56e19303a0c26dfffadc5961317ab3a53b
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-09 11:13:08 -0500

    postgrql

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,45 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, only: [ :create]
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:cocktail_id])
+    @dose.destroy
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
